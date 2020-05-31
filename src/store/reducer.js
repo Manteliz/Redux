a33d27263e5d6d8c5a8483bf0a9dad7eb242b431
Manteliz <mantasvaist@gmail.com>
2020-05-31 12:17:37 +0300

    switch

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,24 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREASE' :
+            return {
+                counter : state.counter + 1
+            };
+        case 'DECREASE' :
+            return {
+                counter : state.counter - 1
+            };
+        case 'ADD' :
+            return {
+                counter : state.counter + action.value
+            };
+        case 'SUBTRACT' :
+            return {
+                counter : state.counter - action.value
+            };
+    }
     return state;
 }
 
